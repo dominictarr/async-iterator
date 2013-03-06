db5f99e610128839ad4e72fa401c9053cdf93505
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-06 18:54:28 +1300

    expose it.ended, queue end cbs

--- index.js
+++ index.js
@@ -1,37 +1,65 @@
 
+function delay (fun) {
+  return function () {
+    var args = [].splice.call(arguments)
+    var self = this
+    process.nextTick(function () {
+      fun.apply(self, args)
+    })
+    return this
+  }
+}
+
 module.exports = function (get, dispose) {
-  var i = 0, calling = false, ended = false
+  var i = 0, calling = false, queueEnd = []
   dispose = dispose || function (cb) {
     return cb()
   }
-
-  return {
+  var it
+  return it = {
+    ended: false,
     next: function (cb) {
       if(calling) return cb(new Error('iterator is currently incrementing'))
-      if(ended)   return cb(new Error('iterator has ended'))
+      if(it.ended)   return cb(new Error('iterator has ended'))
       var async = false
       function forceAsync(fun) {
         if(async) fun()
-        else process.nextTick(fun)
+        else      process.nextTick(fun)
       }
       calling = true
       get.call(this, i ++, function (err, key, value) {
         forceAsync(function () {
           calling = false
-          if(err) ended = true
-          if(key == null && value == null)
-            ended = true
+          if(err || (key == null && value == null))
+            it.ended = true
+
           cb(err, key, value)
+
+          if(queueEnd.length)
+            queueEnd.forEach(function (e) {
+              it.end(e)
+            })
+
+
         })
       })
       async = true
-      return this
+      return it
     },
     end: function (cb) {
-      if(calling) return cb(new Error('iterator is currently incrementing'))
-      ended = true
-      dispose.call(this, cb)
-      return this
+      if(it.ended) return delay(cb).call(it)
+
+      if(calling)
+        return queueEnd.push(cb)
+      it.ended = true
+      var async = false
+      dispose.call(it, function () {
+        var args = [].slice.call(arguments)
+        if(async) cb.apply(it, args)
+        else      delay(cb).apply(it, args) 
+      })
+      async = true
+      return it
     }
   }
 }
